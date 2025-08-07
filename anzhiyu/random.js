e2551e0e5ed46acf0fc66273b9b5e3bf5d1bd3d4
Lixiney <3034246613@qq.com>
2025-08-07 10:38:18 +0800

    Site updated: 2025-08-07 10:38:16

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["posts/98d7f58f/","posts/d708c66/","posts/5fe44501/","posts/b710732f/","posts/bcdf8f30/","posts/2bf21027/","posts/db284c9b/","posts/df8caf15/","posts/3b86160f/","posts/483d9cef/","posts/2dff4083/","posts/afe38b32/","posts/9f9af3f5/","posts/879a70bf/","posts/83c1bda7/","posts/0/","posts/ed2a42f9/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
